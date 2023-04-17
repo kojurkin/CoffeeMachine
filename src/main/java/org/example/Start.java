1e1c0a64f6e4e6d2d840f8cc733c2c343fd7535e
Ilya Kayukin <kojukinia@stud.omsu.ru>
2023-04-17 22:27:14 +0600

    Initial Commit

--- src/main/java/org/example/Start.java
+++ src/main/java/org/example/Start.java
@@ -1,7 +1,7 @@
 package org.example;
 
 public class Start {
-    public static void start() throws Exception {
+    public static void start() throws Exception { // дополнительный способ запуска если мейн по тем или иным причинам не используется
         Storage storage = new Storage();
         CoffeeService service = new CoffeeService(storage);
         Console console = new Console(service);
